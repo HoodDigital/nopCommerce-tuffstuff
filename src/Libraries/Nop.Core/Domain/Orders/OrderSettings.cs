nopSolutions/nopCommerce

﻿8120 Auto cancel unpaid orders

src/Libraries/Nop.Core/Domain/Orders/OrderSettings.cs
@@ -202,4 +202,24 @@ public partial class OrderSettings : ISettings
     /// Gets or sets a value indicating whether to allow customers to cancel orders
     /// </summary>
     public bool AllowCustomersCancelOrders { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether unpaid orders should be canceled automatically
+    /// </summary>
+    public bool AutoCancelUnpaidOrdersEnabled { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating the period of time after order placement, after which unpaid orders should be canceled (in minutes)
+    /// </summary>
+    public int AutoCancelUnpaidOrdersDelay { get; set; }
+
+    /// <summary>
+    /// Gets or sets the list of payment method system names for which automatic cancellation of unpaid orders is disabled
+    /// </summary>
+    public List<string> AutoCancelIgnoredPaymentMethods { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether an auto-canceled order should be added to the shopping cart
+    /// </summary>
+    public bool PutAutoCanceledOrderToShoppingCart { get; set; }
 }

src/Libraries/Nop.Data/Migrations/UpgradeTo500/DataMigration.cs
@@ -1,0 +1,40 @@
+﻿using FluentMigrator;
+using Nop.Core.Domain.ScheduleTasks;
+
+namespace Nop.Data.Migrations.UpgradeTo500;
+
+[NopUpdateMigration("2026-03-31 00:00:00", "5.00", UpdateMigrationType.Data)]
+public class DataMigration : Migration
+{
+    private readonly INopDataProvider _dataProvider;
+
+    public DataMigration(INopDataProvider dataProvider)
+    {
+        _dataProvider = dataProvider;
+    }
+
+    /// <summary>
+    /// Collect the UP migration expressions
+    /// </summary>
+    public override void Up()
+    {
+        //#8120
+        if (!_dataProvider.GetTable<ScheduleTask>().Any(st => string.Compare(st.Type, "Nop.Services.Orders.AutoCancelPaidOrdersTask, Nop.Services", StringComparison.InvariantCultureIgnoreCase) == 0))
+        {
+            _dataProvider.InsertEntity(new ScheduleTask()
+            {
+                Name = "Auto-cancel unpaid orders",
+                Seconds = 600,
+                Type = "Nop.Services.Orders.AutoCancelPaidOrdersTask, Nop.Services",
+                Enabled = true,
+                LastEnabledUtc = DateTime.UtcNow,
+                StopOnError = false
+            });
+        }
+    }
+
+    public override void Down()
+    {
+        //add the downgrade logic if necessary 
+    }
+}

src/Presentation/Nop.Web.Framework/Migrations/UpgradeTo500/SettingMigration.cs
@@ -4,6 +4,7 @@ using Nop.Core.Domain.ArtificialIntelligence;
 using Nop.Core.Domain.Catalog;
 using Nop.Core.Domain.Common;
 using Nop.Core.Domain.Customers;
+using Nop.Core.Domain.Orders;
 using Nop.Core.Domain.Shipping;
 using Nop.Data;
 using Nop.Data.Migrations;
@@ -68,6 +69,12 @@ public class SettingMigration : MigrationBase
         this.SetSettingIfNotExists<StoreInformationSettings, string>(settings => settings.SnapchatLink, string.Empty);
         this.SetSettingIfNotExists<StoreInformationSettings, string>(settings => settings.PinterestLink, string.Empty);
         this.SetSettingIfNotExists<StoreInformationSettings, string>(settings => settings.TumblrLink, string.Empty);
+
+        //#8120
+        this.SetSettingIfNotExists<OrderSettings, bool>(settings => settings.AutoCancelUnpaidOrdersEnabled, false);
+        this.SetSettingIfNotExists<OrderSettings, int>(settings => settings.AutoCancelUnpaidOrdersDelay, 600);
+        this.SetSettingIfNotExists<OrderSettings, List<string>>(settings => settings.AutoCancelIgnoredPaymentMethods, []);
+        this.SetSettingIfNotExists<OrderSettings, bool>(settings => settings.PutAutoCanceledOrderToShoppingCart, false);
     }
 
     public override void Down()

src/Presentation/Nop.Web/Areas/Admin/Infrastructure/Mapper/AdminMapperConfiguration.cs
@@ -1309,7 +1309,11 @@ public partial class AdminMapperConfiguration : Profile, IOrderedMapperProfile
             .ForMember(model => model.ReturnRequestsEnabled_OverrideForStore, options => options.Ignore())
             .ForMember(model => model.TermsOfServiceOnOrderConfirmPage_OverrideForStore, options => options.Ignore())
             .ForMember(model => model.TermsOfServiceOnShoppingCartPage_OverrideForStore, options => options.Ignore())
-            .ForMember(model => model.PrimaryStoreCurrencyCode, options => options.Ignore());
+            .ForMember(model => model.PrimaryStoreCurrencyCode, options => options.Ignore())
+            .ForMember(model => model.AutoCancelUnpaidOrdersEnabled_OverrideForStore, options => options.Ignore())
+            .ForMember(model => model.AutoCancelUnpaidOrdersDelay_OverrideForStore, options => options.Ignore())
+            .ForMember(model => model.AutoCancelIgnoredPaymentMethods_OverrideForStore, options => options.Ignore())
+            .ForMember(model => model.AvailablePaymentMethods, options => options.Ignore());
         CreateMap<OrderSettingsModel, OrderSettings>()
             .ForMember(settings => settings.GeneratePdfInvoiceInCustomerLanguage, options => options.Ignore())
             .ForMember(settings => settings.MinimumOrderPlacementInterval, options => options.Ignore())

src/Presentation/Nop.Web/Areas/Admin/Models/Settings/OrderSettingsModel.cs
@@ -1,4 +1,5 @@
-﻿using Nop.Web.Areas.Admin.Models.Orders;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Nop.Web.Areas.Admin.Models.Orders;
 using Nop.Web.Framework.Models;
 using Nop.Web.Framework.Mvc.ModelBinding;
 
@@ -15,6 +16,8 @@ public partial record OrderSettingsModel : BaseNopModel, ISettingsModel
     {
         ReturnRequestReasonSearchModel = new ReturnRequestReasonSearchModel();
         ReturnRequestActionSearchModel = new ReturnRequestActionSearchModel();
+        AutoCancelIgnoredPaymentMethods = new List<string>();
+        AvailablePaymentMethods = new List<SelectListItem>();
     }
 
     #endregion
@@ -151,6 +154,23 @@ public partial record OrderSettingsModel : BaseNopModel, ISettingsModel
     [NopResourceDisplayName("Admin.Configuration.Settings.Order.AllowCustomersCancelOrders")]
     public bool AllowCustomersCancelOrders { get; set; }
     public bool AllowCustomersCancelOrders_OverrideForStore { get; set; }
+
+    [NopResourceDisplayName("Admin.Configuration.Settings.Order.AutoCancelUnpaidOrdersEnabled")]
+    public bool AutoCancelUnpaidOrdersEnabled { get; set; }
+    public bool AutoCancelUnpaidOrdersEnabled_OverrideForStore { get; set; }
+
+    [NopResourceDisplayName("Admin.Configuration.Settings.Order.AutoCancelUnpaidOrdersDelay")]
+    public int AutoCancelUnpaidOrdersDelay { get; set; }
+    public bool AutoCancelUnpaidOrdersDelay_OverrideForStore { get; set; }
+
+    [NopResourceDisplayName("Admin.Configuration.Settings.Order.AutoCancelIgnoredPaymentMethods")]
+    public IList<string> AutoCancelIgnoredPaymentMethods { get; set; }
+    public bool AutoCancelIgnoredPaymentMethods_OverrideForStore { get; set; }
+    public IList<SelectListItem> AvailablePaymentMethods { get; set; }
+
+    [NopResourceDisplayName("Admin.Configuration.Settings.Order.PutAutoCanceledOrderToShoppingCart")]
+    public bool PutAutoCanceledOrderToShoppingCart { get; set; }
+    public bool PutAutoCanceledOrderToShoppingCart_OverrideForStore { get; set; }
 
     public ReturnRequestReasonSearchModel ReturnRequestReasonSearchModel { get; set; }
 

HoodDigital/nopCommerce-tuffstuff

src/Libraries/Nop.Core/Domain/Orders/OrderSettings.cs
@@ -202,4 +202,24 @@ public partial class OrderSettings : ISettings
     /// Gets or sets a value indicating whether to allow customers to cancel orders
     /// </summary>
     public bool AllowCustomersCancelOrders { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether unpaid orders should be canceled automatically
+    /// </summary>
+    public bool AutoCancelEnabled { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating the period of time after order placement, after which unpaid orders should be canceled (in minutes)
+    /// </summary>
+    public int AutoCancelDelay { get; set; }
+
+    /// <summary>
+    /// Gets or sets the list of payment method system names for which automatic cancellation of unpaid orders is disabled
+    /// </summary>
+    public List<string> AutoCancelIgnoredPaymentMethods { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether an auto-canceled order should be added to the shopping cart
+    /// </summary>
+    public bool AutoCancelRestoreShoppingCart { get; set; }
 }

src/Libraries/Nop.Data/Migrations/UpgradeTo500/DataMigration.cs
@@ -1,0 +1,40 @@
+﻿using FluentMigrator;
+using Nop.Core.Domain.ScheduleTasks;
+
+namespace Nop.Data.Migrations.UpgradeTo500;
+
+[NopUpdateMigration("2026-03-31 00:00:00", "5.00", UpdateMigrationType.Data)]
+public class DataMigration : Migration
+{
+    private readonly INopDataProvider _dataProvider;
+
+    public DataMigration(INopDataProvider dataProvider)
+    {
+        _dataProvider = dataProvider;
+    }
+
+    /// <summary>
+    /// Collect the UP migration expressions
+    /// </summary>
+    public override void Up()
+    {
+        //#8120
+        if (!_dataProvider.GetTable<ScheduleTask>().Any(st => string.Compare(st.Type, "Nop.Services.Orders.AutoCancelOrdersTask, Nop.Services", StringComparison.InvariantCultureIgnoreCase) == 0))
+        {
+            _dataProvider.InsertEntity(new ScheduleTask()
+            {
+                Name = "Auto-cancel unpaid orders",
+                Seconds = 600,
+                Type = "Nop.Services.Orders.AutoCancelOrdersTask, Nop.Services",
+                Enabled = true,
+                LastEnabledUtc = DateTime.UtcNow,
+                StopOnError = false
+            });
+        }
+    }
+
+    public override void Down()
+    {
+        //add the downgrade logic if necessary 
+    }
+}

src/Presentation/Nop.Web.Framework/Migrations/UpgradeTo500/SettingMigration.cs
@@ -4,6 +4,7 @@ using Nop.Core.Domain.ArtificialIntelligence;
 using Nop.Core.Domain.Catalog;
 using Nop.Core.Domain.Common;
 using Nop.Core.Domain.Customers;
+using Nop.Core.Domain.Orders;
 using Nop.Core.Domain.Shipping;
 using Nop.Data;
 using Nop.Data.Migrations;
@@ -68,6 +69,12 @@ public class SettingMigration : MigrationBase
         this.SetSettingIfNotExists<StoreInformationSettings, string>(settings => settings.SnapchatLink, string.Empty);
         this.SetSettingIfNotExists<StoreInformationSettings, string>(settings => settings.PinterestLink, string.Empty);
         this.SetSettingIfNotExists<StoreInformationSettings, string>(settings => settings.TumblrLink, string.Empty);
+
+        //#8120
+        this.SetSettingIfNotExists<OrderSettings, bool>(settings => settings.AutoCancelEnabled, false);
+        this.SetSettingIfNotExists<OrderSettings, int>(settings => settings.AutoCancelDelay, 600);
+        this.SetSettingIfNotExists<OrderSettings, List<string>>(settings => settings.AutoCancelIgnoredPaymentMethods, []);
+        this.SetSettingIfNotExists<OrderSettings, bool>(settings => settings.AutoCancelRestoreShoppingCart, false);
     }
 
     public override void Down()

src/Presentation/Nop.Web/Areas/Admin/Infrastructure/Mapper/AdminMapperConfiguration.cs
@@ -1309,7 +1309,12 @@ public partial class AdminMapperConfiguration : Profile, IOrderedMapperProfile
             .ForMember(model => model.ReturnRequestsEnabled_OverrideForStore, options => options.Ignore())
             .ForMember(model => model.TermsOfServiceOnOrderConfirmPage_OverrideForStore, options => options.Ignore())
             .ForMember(model => model.TermsOfServiceOnShoppingCartPage_OverrideForStore, options => options.Ignore())
-            .ForMember(model => model.PrimaryStoreCurrencyCode, options => options.Ignore());
+            .ForMember(model => model.PrimaryStoreCurrencyCode, options => options.Ignore())
+            .ForMember(model => model.AutoCancelEnabled_OverrideForStore, options => options.Ignore())
+            .ForMember(model => model.AutoCancelDelay_OverrideForStore, options => options.Ignore())
+            .ForMember(model => model.AutoCancelIgnoredPaymentMethods_OverrideForStore, options => options.Ignore())
+            .ForMember(model => model.AutoCancelRestoreShoppingCart_OverrideForStore, options => options.Ignore())
+            .ForMember(model => model.AvailablePaymentMethods, options => options.Ignore());
         CreateMap<OrderSettingsModel, OrderSettings>()
             .ForMember(settings => settings.GeneratePdfInvoiceInCustomerLanguage, options => options.Ignore())
             .ForMember(settings => settings.MinimumOrderPlacementInterval, options => options.Ignore())

src/Presentation/Nop.Web/Areas/Admin/Models/Settings/OrderSettingsModel.cs
@@ -1,4 +1,5 @@
-﻿using Nop.Web.Areas.Admin.Models.Orders;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Nop.Web.Areas.Admin.Models.Orders;
 using Nop.Web.Framework.Models;
 using Nop.Web.Framework.Mvc.ModelBinding;
 
@@ -15,6 +16,8 @@ public partial record OrderSettingsModel : BaseNopModel, ISettingsModel
     {
         ReturnRequestReasonSearchModel = new ReturnRequestReasonSearchModel();
         ReturnRequestActionSearchModel = new ReturnRequestActionSearchModel();
+        AutoCancelIgnoredPaymentMethods = new List<string>();
+        AvailablePaymentMethods = new List<SelectListItem>();
     }
 
     #endregion
@@ -151,6 +154,23 @@ public partial record OrderSettingsModel : BaseNopModel, ISettingsModel
     [NopResourceDisplayName("Admin.Configuration.Settings.Order.AllowCustomersCancelOrders")]
     public bool AllowCustomersCancelOrders { get; set; }
     public bool AllowCustomersCancelOrders_OverrideForStore { get; set; }
+
+    [NopResourceDisplayName("Admin.Configuration.Settings.Order.AutoCancelEnabled")]
+    public bool AutoCancelEnabled { get; set; }
+    public bool AutoCancelEnabled_OverrideForStore { get; set; }
+
+    [NopResourceDisplayName("Admin.Configuration.Settings.Order.AutoCancelDelay")]
+    public int AutoCancelDelay { get; set; }
+    public bool AutoCancelDelay_OverrideForStore { get; set; }
+
+    [NopResourceDisplayName("Admin.Configuration.Settings.Order.AutoCancelIgnoredPaymentMethods")]
+    public IList<string> AutoCancelIgnoredPaymentMethods { get; set; }
+    public bool AutoCancelIgnoredPaymentMethods_OverrideForStore { get; set; }
+    public List<SelectListItem> AvailablePaymentMethods { get; set; }
+
+    [NopResourceDisplayName("Admin.Configuration.Settings.Order.AutoCancelRestoreShoppingCart")]
+    public bool AutoCancelRestoreShoppingCart { get; set; }
+    public bool AutoCancelRestoreShoppingCart_OverrideForStore { get; set; }
 
     public ReturnRequestReasonSearchModel ReturnRequestReasonSearchModel { get; set; }
 